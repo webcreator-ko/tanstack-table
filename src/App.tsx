ed61d0182179f76e3160a6d6f28f709600de8b34
Kotaro <kotaro@KotaronoMacBook-Air.local>
2024-12-11 11:25:10 +0900

    検索機能の追加

--- src/App.tsx
+++ src/App.tsx
@@ -2,6 +2,7 @@ import styles from "./app.module.scss";
 import ComplexHeadTable from "./parts/complexHeadTable";
 import TanstackTable from "./parts/tanstack-table";
 import TanstackTableCheckbox from "./parts/tanstack-table-checkbox";
+import TanstackTableFilter from "./parts/tanstack-table-filter";
 import TanstackTableTwoSort from "./parts/tanstack-table-two-sort";
 
 function App() {
@@ -9,6 +10,7 @@ function App() {
   <div className={styles.wrap}>
    <TanstackTable />
    <TanstackTableCheckbox />
+   <TanstackTableFilter />
    <TanstackTableTwoSort />
    <ComplexHeadTable />
   </div>
